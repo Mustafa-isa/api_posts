c09a8045f7fe03e4a1389ba9d4e921b2d1c5a22c
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-02-15 04:16:19 +0200

    finish project
    dealing with fake api

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,57 @@
 //start request
+//glopal
+let right =document.querySelector(".right")
+let ul =document.querySelector(".ul")
 
-function getData(){
+function renderuser(){
+    axios(`https://jsonplaceholder.typicode.com/users`)
+.then( res =>{
 
-    axios.get(" http://api.aladhan.com/v1/currentDate?zone=Europe/London")
-    .then( res =>{
-console.log(res)
+    let data = res.data
+    for( const datum of data){
+  let li  =document.createElement("li")
+  li.classList ="ele"
 
-    })
-    .catch( () =>{
-        console.log("htere is wrong")
-    })
+
+  li.innerHTML =` <div  class="div" id="${datum.id}>
+  
+  <span class="content">${datum.name}</span>
+  <span class="email">${datum.email}</span>
+  
+  </div>`
+  ul.append(li)
+    }
+
+})
+
 }
-getData()
+//take id
+ul.addEventListener("click" , (e) =>{
+
+   if(e.target.classList =='div'){
+
+
+   console.log( e.target.getAttribute("id"))
+ 
+   } 
+})
+
+
+function renderDataposts(){
+
+    axios(`https://jsonplaceholder.typicode.com/posts`)
+.then( res =>{
+
+    let data = res.data
+    for( const datum of data){
+        let div =document.createElement("div")
+        div.classList ="element"
+div.innerHTML += `     <h2>${datum.title}</h2>
+<p>${datum.body}</p>`
+       right.append(div)
+    }
+
+})
+}
+renderDataposts() // render posts in dom
+renderuser()
